246a88c8d95038070480ba51f3ab35ff97db7c08
Sara <sara_lunden1@hotmail.com>
2025-02-03 13:08:33 +0100

    new commits

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Mitt namn är Sara, denna portfolio skapades under min frontend utbildning för 
 för kursen. Planen är att använda denna i framtiden och bygga vidare på. 
 
 ## Portfolion är skapat med React + Redux, med Vite 
+
+## Package installed 
+react-router-dom
+react-redux toolkit 
+react-icons 
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FiCheck } from "react-icons/fi";  // react-icons imported from react-icons
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='hello-there'>Hello there!</h1>
+      <p className='no-stranger'>Don&apos;t be a stranger, tell me your name</p>
+
+      <span className='interaction-container'>
+
+        <input
+        className='input-field' 
+        type='text'
+        placeholder='Enter name' 
+        value={''}/>
+
+        <button className="submit-button"><FiCheck /></button>
+
+      </span>
     </>
   )
 }
